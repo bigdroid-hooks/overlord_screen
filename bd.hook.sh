4d096d4a674d7ff7705a203be19e298f9d699511
AXON <axonasif@gmail.com>
2021-08-11 18:20:58 +0600

    refactor: Stuff according to new bigdroid design

--- bd.hook.sh
+++ bd.hook.sh
@@ -1,36 +1,26 @@
 #           Variables - BigDroid Internals
 #           - Only for advanced scripting -
 #           ###############################
+
+#   SRC_DIR             % The 'src' dir is the mountpoint of the project operating-system IMAGE.
 #
-#   BASE_DIR            % The root dir where you find files like: bigdroid, bin, src.
-#                       % Hope it explains the rest.
-#               
-#   SRC_DIR             % The 'src' dir under BASE_DIR.
-#   
-#   HOOKS_DIR           % The 'hooks' dir under BASE_DIR.
-#   
-#   HOOK_BASE           % This variable points to the root-dir
-#                       % of a bigdroid every hook which is being run.
-#   
+#   HOOK_DIR            % This variable points to the root-dir
+#                       % of a bigdroid hook which is being run.
+#
 #   MOUNT_DIR           % The parent dir which holds other child mountpoint dirs.
 #                       % Followed by: system, secondary_ramdisk, initial_ramdisk and install_ramdisk.
-#                       % We also have SYSTEM_MOUNT_DIR, SECONDARY_RAMDISK_MOUNT_DIR
-#                       % INITIAL_RAMDISK_MOUNT_DIR and INSTALL_RAMDISK_MOUNT_DIR variables.
-#   
-#   ISO_DIR             % Your configured ISO by the '--setup-iso' argument is cached in this dir.
-#   
-#   BUILD_DIR           % This dir is used as the temporary boilerplate while building an ISO/IMAGE.
-#   
+#                       % Use `SYSTEM_MOUNT_DIR`, `SECONDARY_RAMDISK_MOUNT_DIR`
+#                       % `INITIAL_RAMDISK_MOUNT_DIR`, `INSTALL_RAMDISK_MOUNT_DIR` variables for path.
+#
 #   TMP_DIR             % You can use this dir for storing temporary files.
-#                       % It's more like '/tmp' as in your linux distro.
+#                       % It's equivalent '/tmp' dir but for bigdroid hooks.
 #
-#   OVERLAY_DIR         % The overlay dir under BASE_DIR
-#   
-#   DISTRO_NAME         % Your android distro name as per 'hooks/distro.sh' or the defaults.
-#   
-#   DISTRO_VERSION      % Your android distro version as per 'hooks/distro.sh' or the defaults.
+#   SECONDARY_RAMDISK   % This is either true or false aka a bolean.
+#                       % Depends on whether the project operating-system has a ramdisk.img
 #
-#   @@ Protip: Take a look at 'src/main.sh'.
+#   SYSTEM_IMAGE        % This points to the project system image (system.sfs or system.img) file.
+#
+#   @@ Tip: Also all the varaibles defined in the project `Bigdroid.meta` can be used.
 
 
 
@@ -38,12 +28,12 @@
 #              - For easy hooks scripting -
 #           ##################################
 #
-#   gclone              % Copy files preserving all their attrs with progress indicator.
-#                       % Example: `gclone "$HOOK_BASE/myfile.txt" "$SYSTEM_DIR/lib64"`
-#   
+#   gclone              % Copy(rsync) files preserving all their attrs with progress indicator.
+#                       % Example: `gclone "$HOOK_DIR/myfile.so" "$SYSTEM_DIR/lib64"`
+#
 #   wipedir             % Easily wipe/empty a dir(childs) without removing it's parent.
-#                       % Example: `wipedir "$MOUNT_DIR_INSTALL_RAMDISK/grub"`
-#   
+#                       % Example: `wipedir "$INSTALL_RAMDISK_MOUNT_DIR/grub"`
+#
 #   @@ Protip: Take a look at 'src/utils.sh'
 #
 #
@@ -53,21 +43,10 @@
 #     - Some native gearlock vars and functions -
 #           ###############################
 #
-#   %% Simply take a look at 'src/libgearlock.sh' to know 
+#   %% Simply take a look at 'https://github.com/bigdroid/bigdroid/blob/main/src/libgearlock.sh' to know
 #   %% which gearlock variables and functions are available for use.
 
 
-
-#               An example hook script
-#           - To give you some quick idea -
-#           ###############################
-
-## Okay so here we go...
-## Basically if you wanna start with this example script,
-## then copy this `example_hook` dir into `hooks/` dir.
-## Then start up editing it's `bd.hook.sh`.
-
-# Now I want to merge my own hw files there from my HOOK_BASE (hooks/example_hook).
 ADD_CUSTOM_GHOME_HOOK_BASE="$(hook::fetch_path "custom_ghome")/ghome/dependencies/assets/essential-user-apps"
 gclone "$HOOK_BASE/app.apk" "$ADD_CUSTOM_GHOME_HOOK_BASE/OVERLORD.apk" || exit
 # Now let's wait in a subprocess until `add_custom_ghome` is done
